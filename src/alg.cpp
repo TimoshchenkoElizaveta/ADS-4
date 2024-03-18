5fa0de525ab43fae1072f229134ecb54530c16ac
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-03-18 20:05:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,15 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            } else if (arr[i] + arr[j] > value) {
+                break;
+            }
+        }
+    }
   return 0;
 }
 int countPairs2(int *arr, int len, int value) {
