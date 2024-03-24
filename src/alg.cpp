cec9e3bdb09f6cf2aa6e0a310692c6203ef100e8
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-03-24 20:29:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,35 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-    int cout = 0;
-    while (cout < 1000000) {
-        cout++;
-    }
     int count = 0;
     for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; ++j) {
-            if ((arr[i] + arr[j]) == value)
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
                 count++;
+            }
         }
     }
     return count;
 }
 int countPairs2(int *arr, int len, int value) {
-    int found = 0;
-    int first = 0;
-    int last = size - 1;
-    int middle = 0;
-    while (first <= last) {
-        middle = (first + last) / 2;
-        if (arr[middle] == value) {
-            ++found;
-            int index = middle - 1;
-            while (index >= first && arr[index] == value) {
-                 ++found;
-                 --index;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (i != j && arr[i] + arr[j] == value) {
+                count++;
             }
-            index = middle + 1;
-            while (index <= last && arr[index] == value) {
-                ++found;
-                ++index;
-            }
-            return found;
-        } else if (arr[middle] < value) {
-            first = middle + 1;
-        } else {
-            last = middle - 1;
         }
     }
-    return 0;
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-    int i = 0, r = 0;
-    while (arr[i] <= (value / 2) -1) {
-        int num = value - arr[i];
-        r += cbinsearch(arr, len, num);
-        i++;
+    int count = 0;
+    int freq[1001] = {0};
+    for (int i = 0; i < len; i++) {
+        int complement = value - arr[i];
+        if (complement >= 0 && complement <= 1000) {
+            count += freq[complement];
+        }
+        freq[arr[i]]++;
     }
-    int mid = cbinsearch(arr, len, value / 2);
-    r += mid * (mid - 1) / 2;
-    return r;
+    return count;
 }
