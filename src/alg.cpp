40a782d73daacfcaa5651a9d1d819ae57d3fe24f
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-03-24 19:44:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,54 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[i] + arr[j] == value)
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right)
+    {
+        int sum = arr[left] + arr[right];
+        if (sum == value)
+        {
+            count++;
+            left++;
+            right--;
+        }
+        else if (sum < value)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int hash = new int[value + 1]();
+    for (int i = 0; i < len; i++)
+    {
+        if (hash[value - arr[i]] == 1)
+        {
+            count++;
+        }
+        hash[arr[i]] = 1;
+    }
+    delete[] hash;
+    return count;
 }
